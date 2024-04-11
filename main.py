2b098c032b5cf9287ead639ce2b8b097bf347110
Bashar <bhamdanieh@gmail.com>
2024-04-11 15:51:04 +0300

    Add sound feature in T2M mode

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import time
+import pygame
+
 
 data = pd.read_csv('morse_code.csv')
 
@@ -33,6 +36,22 @@ def get_text(code : str):
                 pass
     return "".join(text)
 
+def play_morse_code(morse_code):
+    pygame.mixer.init()
+    frequency = 440  # Set the frequency of the tone (440 Hz is standard)
+    duration_short = 400  # Duration of short beep in milliseconds
+    duration_long = 900  # Duration of long beep in milliseconds
+    volume = 0.5  # Set the volume (0.0 to 1.0)
+
+    for char in morse_code:
+        if char == '.':
+            pygame.mixer.Sound.play(pygame.mixer.Sound("sound/short_beep.wav"))
+            time.sleep(duration_short / 1000)
+        elif char == '-':
+            pygame.mixer.Sound.play(pygame.mixer.Sound("sound/long_beep.wav"))
+            time.sleep(duration_long / 1000)
+        elif char == ' ':
+            time.sleep(duration_short / 1000)  # Pause between letters
 
 def main():
     is_on = True
@@ -46,6 +65,18 @@ def main():
             message    = input("What is your text message? ")
             morse_code = get_code(message)
             print(f"Morse Code: {morse_code}")
+            
+            play_sound = False
+            while not play_sound:
+                play_code = input("Do you want to play the code sound (Y or N)? ").upper()
+                if play_code == "Y":
+                    play_morse_code(morse_code)
+                    play_sound = True
+                elif play_code == "N":
+                    main()
+                else:
+                    play_sound=False
+
         elif morse_or_text == "Q":
             is_on = False
         else:
